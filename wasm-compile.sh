5d812cfd2e4c6c492a8838f970dcf3b288f8c2e2
Khongchai <world1955@hotmail.com>
2026-06-13 23:13:34 +0700

    added wasm-compile methods

--- wasm-compile.sh
+++ wasm-compile.sh
@@ -1,5 +1,5 @@
 emcc -O3 .libs/libopus.a \
   -s WASM=1 \
-  -s EXPORTED_FUNCTIONS="['_opus_encoder_create', '_opus_encoder_destroy', '_opus_decoder_destroy', '_opus_encode_float', '_opus_decoder_create', '_opus_decode', '_malloc', '_free']" \
+  -s EXPORTED_FUNCTIONS="['_opus_encoder_create', '_opus_encoder_destroy', '_opus_decoder_destroy', '_opus_encode_float', '_opus_decoder_create', '_opus_decode', '_malloc', '_free', '_opus_encoder_ctl']" \
   -s EXPORTED_RUNTIME_METHODS=ccall,cwrap,HEAPF32,HEAPU8,HEAP16 \
   -o opus.js
