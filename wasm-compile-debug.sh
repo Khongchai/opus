44ece3a96231b7da13743a91b4e9a0f024d9d299
Khongchai <world1955@hotmail.com>
2026-06-15 13:50:41 +0700

    added debug flag to opus debug compilation script

--- wasm-compile-debug.sh
+++ wasm-compile-debug.sh
@@ -1,4 +1,4 @@
-emcc -O0 .libs/libopus.a \
+emcc -O0 -g .libs/libopus.a \
   -s MODULARIZE=1 -s EXPORT_NAME="createOpusModule" \
   -s EXPORTED_FUNCTIONS="['_opus_encoder_create', '_opus_encoder_destroy', '_opus_decoder_destroy', '_opus_encode_float', '_opus_decoder_create', '_opus_decode', '_malloc', '_free', '_opus_encoder_ctl', '_opus_decode_float']" \
   -s EXPORTED_RUNTIME_METHODS=ccall,cwrap,HEAPF32,HEAPU8,HEAP16,HEAP32  \
