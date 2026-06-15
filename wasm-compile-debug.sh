3a406e8b950446053c0addc572a8577ec03e737e
Khongchai <world1955@hotmail.com>
2026-06-15 13:40:20 +0700

    more compilation script update

--- wasm-compile-debug.sh
+++ wasm-compile-debug.sh
@@ -1,5 +1,4 @@
-emcc -Os .libs/libopus.a \
-  -s WASM=1 \
+emcc -O0 .libs/libopus.a \
   -s EXPORTED_FUNCTIONS="['_opus_encoder_create', '_opus_encoder_destroy', '_opus_decoder_destroy', '_opus_encode_float', '_opus_decoder_create', '_opus_decode', '_malloc', '_free', '_opus_encoder_ctl', '_opus_decode_float']" \
-  -s EXPORTED_RUNTIME_METHODS=ccall,cwrap,HEAPF32,HEAPU8,HEAP16 \
+  -s EXPORTED_RUNTIME_METHODS=ccall,cwrap,HEAPF32,HEAPU8,HEAP16,HEAP32  \
   -o opus.js
